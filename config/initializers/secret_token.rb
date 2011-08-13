fad64581282203aa8838ff74c1d9407cd9288b0d
Nick Malcolm <nick@southgatelabs.com>
2011-08-14 02:51:02 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapuaki::Application.config.secret_token = 'b18a5d1c13d37df5579899a9b56e84d67fdd2927d00d0ff7d4fb8e9c4388083963075b92fce2e2dfb76097e4602abbae5aa4c6bd3131beb68cc36e5e1131b650'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Tapuaki::Application.routes.draw do
+  
+  root :to => "home#index"
+  
+end
