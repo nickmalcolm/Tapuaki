3c2f470cb89184eeda5859baacd57dd9b3ae3b3f
Nick Malcolm <nick@southgatelabs.com>
2011-09-05 23:21:35 +1200

    Add subtitle to appeals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110819022642) do
+ActiveRecord::Schema.define(:version => 20110905104618) do
 
   create_table "appeals", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20110819022642) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "subtitle"
   end
 
 end
