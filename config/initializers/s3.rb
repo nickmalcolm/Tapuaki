0a0b01e5761db6d1c63d92c74b6119f2e8afe0fc
Nick Malcolm <nick@southgatelabs.com>
2011-09-09 01:16:06 +1200

    Fix bucket problem

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,7 +3,7 @@ if Rails.env == "production"
   S3_CREDENTIALS = { 
     :access_key_id => ENV['S3_KEY'], 
     :secret_access_key => ENV['S3_SECRET'], 
-    :bucket => "sharedearth-production"}
+    :bucket => "tapuaki_production"}
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
